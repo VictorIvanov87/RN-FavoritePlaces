063398ad2767463da257a3b13b394d39aa8f6e69
Victor Hristov <victor.hristov@mentormate.com>
2023-10-30 09:21:35 +0200

    first commit

---
+++ screen/AddPlaces.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const AddPlaces = () => {
+	return (
+		<View>
+			<Text>AddPlaces</Text>
+		</View>
+	);
+};
+
+export default AddPlaces;
---
+++ screen/AllPlaces.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const AllPlaces = () => {
+	return (
+		<View>
+			<Text>AllPlaces</Text>
+		</View>
+	);
+};
+
+export default AllPlaces;
